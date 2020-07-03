ec275ac0849930051d48caace7f7dc912e4517ef
Thomas Hillenbrand <tomhillenbrand16@gmail.com>
2020-07-02 22:20:01 -0400

    Implementing BeerController. Closes #3

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -16,19 +16,15 @@ import java.util.UUID;
 public class BeerDto {
 
     private UUID id;
+    private long upc;
     private Integer version;
+
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private BigDecimal price;
+    private Integer quantityOnHand;
 
     private OffsetDateTime createdDate;
     private OffsetDateTime latModifiedDate;
-
-    private String beerName;
-
-    private BeerStyleEnum beerStyle;
-
-    private long upc;
-
-    private BigDecimal price;
-
-    private Integer quantityOnHand;
 
 }
