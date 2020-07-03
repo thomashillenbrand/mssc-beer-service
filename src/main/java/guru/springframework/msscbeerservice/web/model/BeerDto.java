384c53bd6c6693fc402345cfca5ab19c98e6aeb4
Thomas Hillenbrand <tomhillenbrand16@gmail.com>
2020-07-02 22:00:06 -0400

    Creating Data Model for Beer Service. Closes #2

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,34 @@
+package guru.springframework.msscbeerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+
+    private UUID id;
+    private Integer version;
+
+    private OffsetDateTime createdDate;
+    private OffsetDateTime latModifiedDate;
+
+    private String beerName;
+
+    private BeerStyleEnum beerStyle;
+
+    private long upc;
+
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
+
+}
