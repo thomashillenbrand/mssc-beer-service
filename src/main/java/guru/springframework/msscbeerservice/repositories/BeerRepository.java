180a8ccb4d1a60660fd5ce540882edc815cb3a71
Thomas Hillenbrand <tomhillenbrand16@gmail.com>
2020-07-16 22:38:29 -0400

    Added beer bootstrap-- closes #7

--- src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -6,8 +6,4 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import java.util.UUID;
 
 public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
-
-
-
-
 }
